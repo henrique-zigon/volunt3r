554e82fef6e2b51c0b3da1cbe857a5247bdd5b20
gabrielronny <gabrielronny123@hotmail.com>
2021-03-01 18:39:41 -0300

    projeto api iniciado

---
+++ api-voluntier/src/main/java/br/com/voluntier/apivoluntier/ApiVoluntierApplication.java
@@ -0,0 +1,13 @@
+package br.com.voluntier.apivoluntier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVoluntierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVoluntierApplication.class, args);
+	}
+
+}
