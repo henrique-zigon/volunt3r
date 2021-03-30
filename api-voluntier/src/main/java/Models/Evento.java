db51930d1c79fe333d445612d2fe68306180f1dc
gabrielronny <gabrielronny123@hotmail.com>
2021-03-30 12:57:37 -0300

    correção do atributo(dataEvento)

--- api-voluntier/src/main/java/Models/Evento.java
+++ api-voluntier/src/main/java/Models/Evento.java
@@ -5,11 +5,21 @@ import java.util.List;
 public class Evento extends Postagem {
 
     private String categoria;
+    private String dataEvento;
+
+    public String getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(String dataEvento) {
+        this.dataEvento = dataEvento;
+    }
 
     public Evento(String titulo, String descricao, String dataPostagem, String pathImagem, int cliques, Usuario usuario,
-                  List<String> tagsPostagem, String categoria) {
+                  List<String> tagsPostagem, String categoria, String dataEvento) {
         super(titulo, descricao, dataPostagem, pathImagem, cliques, usuario, tagsPostagem);
         this.categoria = categoria;
+        this.dataEvento = dataEvento;
     }
 
     public String getCategoria() {
