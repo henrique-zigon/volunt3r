11adfbee74f36edb17aa88320dcf615300ad6745
gabrielronny <gabrielronny123@hotmail.com>
2021-05-06 20:20:11 -0300

    CRUD publicacao/evento adicionado

--- api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Controllers/EventoController.java
+++ api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Controllers/EventoController.java
@@ -1,12 +1,10 @@
 package br.com.voluntier.apivoluntier.Controllers;
 
 
-import br.com.voluntier.apivoluntier.Models.Evento;
 import br.com.voluntier.apivoluntier.Models.Publicacao;
-import br.com.voluntier.apivoluntier.Models.UsuarioEvento;
 import br.com.voluntier.apivoluntier.Repositories.EventoRepository;
 import br.com.voluntier.apivoluntier.Repositories.PublicacaoRepository;
-import br.com.voluntier.apivoluntier.Repositories.UsuarioEventoRepository;
+import br.com.voluntier.apivoluntier.Repositories.InscricaoEventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +20,7 @@ public class EventoController {
     EventoRepository repository;
 
     @Autowired
-    UsuarioEventoRepository repositoryUsuarioEvento;
+    InscricaoEventoRepository repositoryInscricaoEvento;
 
     @Autowired
     PublicacaoRepository repositoryPublicacao;
@@ -34,20 +32,6 @@ public class EventoController {
         return ResponseEntity.status(200).body(repositoryPublicacao.findAllIdEventoNotNull());
     }
 
-
-//    @PostMapping("/novo")
-//    public ResponseEntity postEvento(@RequestBody Evento novoEvento) {
-//        retornoHasmap.clear();
-//        try {
-//            //repository.save(novoEvento);
-//            retornoHasmap.put("message", "Evento criado com sucesso!");
-//
-//            return ResponseEntity.status(201).body(repository.findEventoMaxId().get(0).getId());
-//        } catch (Exception e) {
-//            return ResponseEntity.status(500).build();
-//        }
-//    }
-
     @PostMapping("/novo")
     public ResponseEntity postPublicacaoEvento(@RequestBody Publicacao novaPublicacaoEvento) {
         try {
@@ -55,16 +39,16 @@ public class EventoController {
             retornoHasmap.put("message", "Evento criado com sucesso!");
             return ResponseEntity.status(201).body(retornoHasmap);
         } catch (Exception e) {
-            return ResponseEntity.status(401).build();
+            return ResponseEntity.status(500).build();
         }
 
     }
 
     @PostMapping("/inscrever")
-    public ResponseEntity postUsuarioEvento(@RequestBody UsuarioEvento novaInscricao) {
+    public ResponseEntity postUsuarioEvento(@RequestBody InscricaoEventoRepository novaInscricao) {
         retornoHasmap.clear();
         try {
-            repositoryUsuarioEvento.save(novaInscricao);
+            //repositoryInscricaoEvento.save(novaInscricao);
             retornoHasmap.put("message", "usuário inscrito com sucesso!");
             return ResponseEntity.status(201).body(retornoHasmap);
         } catch (Exception e) {
