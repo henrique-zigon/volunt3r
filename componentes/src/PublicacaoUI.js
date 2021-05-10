4dae549ea7a95f92ca8e67158f89897c5143f288
giovanna-oliveira <giovanna.oliveira@bandtec.com.br>
2021-05-09 21:41:10 -0300

    publicacao 80%

--- componentes/src/PublicacaoUI.js
+++ componentes/src/PublicacaoUI.js
@@ -5,12 +5,12 @@ import "./publicacao-style.css";
 const Publicacao = props => {
     return (
         <div className="publicacao text-center shadow">
-            <div className="publicacao-info">
-                <div className="info-publicacao">
+            <div className="publicacao-main">
+                <div className="publicacao-info">
                     <div className="user-img">
                         <img src={props.imgIconUser} className="user-img"></img></div>
                     <div className="publicacao-owner"> {props.userName} </div>
-                    <div className="publicacao-time text-secondary"> {props.title} </div>
+                    <div className="publicacao-time text-secondary"> {props.time} </div>
                     <div className="publicacao-actions">
                         <div className="publicacao-icons">
                             <img src={props.imgIconLike} className="publicacao-icon"></img>
@@ -22,10 +22,8 @@ const Publicacao = props => {
                         </div>
                     </div>
                 </div>
-                <div className="publicacao-main">
-                    <div className="overflow">
+                <div className="publicacao-content">
                         <img src={props.imgsrc} className="publicacao-img"></img>
-                    </div>
                     <div className="publicacao-text text-secondary">
                         <div className="publicacao-desc">{props.desc}</div>
                         <div className="publicacao-tags">{props.tags}</div>
