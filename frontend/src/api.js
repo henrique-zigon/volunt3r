f82b0a4c4bdeead48ab36f8e820cc89909a72c41
gabrielronny <gabrielronny123@hotmail.com>
2021-06-09 10:48:57 -0300

    url da api na azure

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-const api=axios.create({
-    baseURL:"http://localhost:3001/"
+const api = axios.create({
+    baseURL:"https://api-volunt3r.azurewebsites.net/"
 })
 
 export default api;
