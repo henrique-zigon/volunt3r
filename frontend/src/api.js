bd1802802e488223131d9d65cba1a1995cd08cb1
gabrielronny <gabrielronny123@hotmail.com>
2021-06-29 16:21:53 -0300

    url api alterada para maq azure

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL:"https://api-volunt3r.azurewebsites.net/"
-    baseURL: "http://localhost:3001/"
+    // baseURL: "http://localhost:3001/"
+    baseURL: "http://voluntier.eastus.cloudapp.azure.com"
 })
 
 export default api;
