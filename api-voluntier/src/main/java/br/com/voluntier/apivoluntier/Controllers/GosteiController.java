430019adfd4de0fd37c5a5e6bfe5e9f3873bab4e
henrique-zigon <61800698+henrique-zigon@users.noreply.github.com>
2021-08-26 00:06:15 -0300

    Adicionando função de retirar curtida
    Para realizar o descurtir, tem que passar o "id" do Gostei

--- api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Controllers/GosteiController.java
+++ api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Controllers/GosteiController.java
@@ -4,12 +4,10 @@ import br.com.voluntier.apivoluntier.Models.Gostei;
 import br.com.voluntier.apivoluntier.Repositories.GosteiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+
 
 @RestController
 @RequestMapping("/gostei")
@@ -17,9 +15,16 @@ public class GosteiController {
 
     @Autowired
     private GosteiRepository repository;
+
+
     private HashMap<String, Object> retornoHasmap = new HashMap<>();
 
-    @PostMapping("/novo")
+    @GetMapping
+    public ResponseEntity getGostei(){
+        return ResponseEntity.status(200).body(repository.findAll());
+    }
+
+    @PostMapping
     public ResponseEntity postGostei(@RequestBody Gostei novoGostei) {
         retornoHasmap.clear();
         try {
@@ -29,6 +34,19 @@ public class GosteiController {
         } catch (Exception e) {
             return ResponseEntity.status(500).build();
         }
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteGostei(@RequestBody Gostei gosteiAtingido){
+        retornoHasmap.clear();
+        try{
+            repository.delete(gosteiAtingido);
+            retornoHasmap.put("message", "Gostei removido com sucesso!");
+            return ResponseEntity.status(201).body(retornoHasmap);
+        }catch (Exception e){
+            return ResponseEntity.status(500).build();
+        }
+
     }
 
 }
