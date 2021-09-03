83d72c666d2a416083c4407926f8e8efa3fbeb51
henrique-zigon <61800698+henrique-zigon@users.noreply.github.com>
2021-09-03 19:48:59 -0300

    Ajustes nos end-points de gostei

--- api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Controllers/GosteiController.java
+++ api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Controllers/GosteiController.java
@@ -1,12 +1,18 @@
 package br.com.voluntier.apivoluntier.Controllers;
 
 import br.com.voluntier.apivoluntier.Models.Gostei;
+import br.com.voluntier.apivoluntier.Models.Publicacao;
+import br.com.voluntier.apivoluntier.Models.Usuario;
 import br.com.voluntier.apivoluntier.Repositories.GosteiRepository;
+import br.com.voluntier.apivoluntier.Repositories.PublicacaoRepository;
+import br.com.voluntier.apivoluntier.Repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -15,7 +21,10 @@ public class GosteiController {
 
     @Autowired
     private GosteiRepository repository;
-
+    @Autowired
+    private PublicacaoRepository repositoryPublicacao;
+    @Autowired
+    private UsuarioRepository repositoryUsuario;
 
     private HashMap<String, Object> retornoHasmap = new HashMap<>();
 
@@ -27,10 +36,31 @@ public class GosteiController {
     @PostMapping
     public ResponseEntity postGostei(@RequestBody Gostei novoGostei) {
         retornoHasmap.clear();
+        Optional<Gostei> gosteiExistente;
+        Optional<Publicacao> publiExistente;
+        Optional<Usuario> usuExistente;
+        try {
+            gosteiExistente=repository.findAllGosteiByFKs(novoGostei.getFkPublicacao().getId(),novoGostei.getFkUsuario().getIdUsuario());
+            publiExistente=repositoryPublicacao.findById(novoGostei.getFkPublicacao().getId());
+            usuExistente=repositoryUsuario.findById(novoGostei.getFkUsuario().getIdUsuario());
+        }
+        catch (Exception e){
+            return  ResponseEntity.status(500).body("Parâmetros incorretos ou inexistentes");
+        }
+
+        if(!publiExistente.isPresent() || !usuExistente.isPresent()){
+            retornoHasmap.put("message","Gostei sem parâmetros");
+            return ResponseEntity.status(400).body(retornoHasmap);
+        } else if(gosteiExistente.isPresent()) {
+            retornoHasmap.put("message", "Você ja gostou dessa publicação");
+            return ResponseEntity.status(400).body(retornoHasmap);
+        }
+
         try {
             repository.save(novoGostei);
             retornoHasmap.put("message", "Gostei registrado com sucesso!");
             return ResponseEntity.status(201).body(retornoHasmap);
+
         } catch (Exception e) {
             return ResponseEntity.status(500).build();
         }
@@ -40,8 +70,20 @@ public class GosteiController {
     public ResponseEntity deleteGostei(@RequestBody Gostei gosteiAtingido){
         retornoHasmap.clear();
         try{
-            repository.delete(gosteiAtingido);
-            retornoHasmap.put("message", "Gostei removido com sucesso!");
+            List<Optional<Gostei>>gosteiExistente;
+            try {
+                gosteiExistente=repository.findAllGosteiByFKs1(gosteiAtingido.getFkPublicacao().getId(),gosteiAtingido.getFkUsuario().getIdUsuario());
+            }catch (Exception e1){
+                return ResponseEntity.status(500).body(e1.getMessage());
+            }
+            if (gosteiExistente.size()>1){
+                retornoHasmap.put("message","duplicidade de gostei");
+            } else if (!gosteiExistente.isEmpty()){
+                repository.delete(gosteiExistente.get(0).get());
+                retornoHasmap.put("message", "Gostei removido com sucesso!");
+            }else {
+                retornoHasmap.put("message","Gostei não encontrado");
+            }
             return ResponseEntity.status(201).body(retornoHasmap);
         }catch (Exception e){
             return ResponseEntity.status(500).build();
--- api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Repositories/GosteiRepository.java
+++ api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Repositories/GosteiRepository.java
@@ -2,9 +2,19 @@ package br.com.voluntier.apivoluntier.Repositories;
 
 import br.com.voluntier.apivoluntier.Models.Gostei;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface GosteiRepository extends JpaRepository<Gostei, Integer> {
 
     long countByFkPublicacao(int id);
+
+    @Query(value = "select * from Gostei where fk_publicacao =?1 and fk_usuario=?2",nativeQuery = true)
+    List<Optional<Gostei>> findAllGosteiByFKs1(int fk_publicacao, int fk_usuario);
+
+    @Query(value = "select * from Gostei where fk_publicacao =?1 and fk_usuario=?2",nativeQuery = true)
+    Optional<Gostei> findAllGosteiByFKs(int fk_publicacao, int fk_usuario);
 
 }
