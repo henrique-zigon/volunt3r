6c23d1fadc44c22cde20b5fbab559271a504827d
gabrielronny <gabrielronny123@hotmail.com>
2021-09-26 23:40:52 -0300

    m

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const api = axios.create({
     //baseURL:"https://api-volunt3r.azurewebsites.net/"
     // baseURL: "http://localhost:3001/"
-    baseURL: "http://voluntier.eastus.cloudapp.azure.com:81"
+    // baseURL: "http://voluntier.eastus.cloudapp.azure.com:81"
+    baseURL: "http://44.192.103.118:81"
 })
 
 export default api;
