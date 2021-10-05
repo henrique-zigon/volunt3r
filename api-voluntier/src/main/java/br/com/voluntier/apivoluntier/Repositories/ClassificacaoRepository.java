c68939964283ff77b893c2a2e8fc19a5321777e8
aluno <aluno@bandtec.com>
2021-10-04 21:28:15 -0300

    bonificacao de milhas começo

--- api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Repositories/ClassificacaoRepository.java
+++ api-voluntier/src/main/java/br/com/voluntier/apivoluntier/Repositories/ClassificacaoRepository.java
@@ -33,4 +33,30 @@ public interface ClassificacaoRepository extends JpaRepository<Classificacao,Str
             ") sub2 on categoria.nome_categoria = sub2.nome_categoria;\n",nativeQuery = true)
     List<Classificacao> FindAllByFkUsuarioAndfkCategoria(int fkUsuario);
 
+
+
+    @Query(value = "select categoria.*,\n" +
+            "       CASE\n" +
+            "           WHEN cont is null then 0\n" +
+            "           ELSE cont\n" +
+            "        END contagem,\n" +
+            "       CASE\n" +
+            "           WHEN cont >= limite_ouro THEN 'OURO'\n" +
+            "           WHEN cont >= limite_prata THEN 'PRATA'\n" +
+            "           WHEN cont >= limite_bronze THEN 'BRONZE'\n" +
+            "           ELSE 'Sem ranque'\n" +
+            "END ranque\n" +
+            "    from categoria\n" +
+            "left join (\n" +
+            "    select nome_categoria,\n" +
+            "           count(*) as cont\n" +
+            "    from (\n" +
+            "        select * from inscricao_evento\n" +
+            "        left join evento on fk_evento = id_evento\n" +
+            "        left join categoria on id_categoria = fk_categoria\n" +
+            "        where fk_usuario = ?1\n" +
+            "    ) sub\n" +
+            "    group by nome_categoria\n" +
+            ") sub2 on categoria.nome_categoria = sub2.nome_categoria where id_categoria=?2;\n",nativeQuery = true)
+    Classificacao FindAllByFkUsuarioAndfkCategoriaBonificacao(int fkUsuario,int fkCategoria);
 }
