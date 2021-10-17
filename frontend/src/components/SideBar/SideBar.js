47cd6538f94b29c03034415938c6fbe22a610303
gabrielronny <gabrielronny123@hotmail.com>
2021-10-17 18:38:07 -0300

    ajustes na sidebar e no estilo da dashboard

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { Link, useLocation, useHistory } from 'react-router-dom';
-import { BiLineChart, BiExit, BiDetail } from 'react-icons/bi';
+import React, { useState } from 'react';
 import avatarPadrao from '../../images/avatar_padrao.png';
-
+import { Link, useLocation, useHistory } from 'react-router-dom';
 
 import './sidebar.css';
 import { useCookies } from 'react-cookie';
+import ItemSideBar from './ItemSideBar';
+
+import {
+  BiLineChart,
+  BiExit,
+  BiDetail,
+  BiChevronDown,
+  BiAddToQueue,
+  BiUser,
+  BiListUl
+} from 'react-icons/bi';
 
 const SideBar = props => {
 
   let location = useLocation().pathname;
 
   const imageUser = props.userpic == null ? avatarPadrao : props.userpic;
-  const [cookies, setCookie, removeCookie]= useCookies(['volunt3r', 'volunt3r_user']);
-  const history =  useHistory();
+  const [cookies, setCookie, removeCookie] = useCookies(['volunt3r', 'volunt3r_user']);
+  const history = useHistory();
+
+
+  const [openSubItem, setOpenSubItem] = useState("");
+
+  function openSubItemSideBar() {
+    if (openSubItem === "") {
+      setOpenSubItem("open")
+    } else {
+      setOpenSubItem("")
+    }
+
+  }
 
   function sair() {
     removeCookie('volunt3r')
@@ -25,34 +46,90 @@ const SideBar = props => {
 
   return (
     <div className="sidebar">
+      <header>
 
-      <Link className="exit-icon" onClick={sair}>
-        <BiExit size={ 25 } />
-      </Link>
+        <div className="userpic">
+          <img
+            src={imageUser}
+            alt="Foto do usuário X"
+          />
+        </div>
+        <Link className="exit-icon" onClick={sair}>
+          <BiExit size={25} />
+        </Link>
+      </header>
 
-      <div className="userpic">
-        <img
-          src={imageUser}
-          alt="Foto do usuário X"
-        />
-      </div>
+      {/*       
       <span className="username">{props.username}</span>
-      <span className="useremail">{props.useremail}</span>
+      <span className="useremail">{props.useremail}</span> */}
 
       <div className="sidebar-itens">
-        <Link to="/dashboard" className={
-          location === "/dashboard" ? "item current": "item" 
-        }>
-          <BiLineChart className="icon" size={20} />
-          <span>Dashboard</span>
+
+        {/* <ItemSideBar title="Dashboard" currentLocation={location}/> */}
+
+
+        <Link to="/dashboard" className="item">
+          <div className="container-item">
+            <div className="" className={location === "/dashboard" ? "principal-item current" : "principal-item"}>
+              <div className="group">
+                <BiLineChart className="icon" size={20} />
+                <span>Dashboard</span>
+              </div>
+            </div>
+          </div>
         </Link>
 
-        <Link to="/dashboard/relatorios" className={
+        <Link className="item">
+          <div className="container-item">
+            <div className="principal-item" onMouseEnter={openSubItemSideBar}>
+              <div className="group">
+                <BiUser className="icon" size={20} />
+                <span>Gerência dos Usuários</span>
+              </div>
+              <BiChevronDown className="icon icon-left" size={20} />
+            </div>
+            <div className={`sub-itens ${openSubItem} `} >
+              <Link className="sub-item">
+                <BiAddToQueue className="icon" size={20} />
+                <span>Criar Usuário</span>
+              </Link>
+
+              <Link className="sub-item">
+                <BiListUl className="icon" size={20} />
+                <span>Meus Usuários</span>
+              </Link>
+            </div>
+          </div>
+        </Link>
+
+        <Link to="/dashboard/relatorios" className="item">
+          <div className="container-item">
+            <div className="" className={location === "/dashboard/relatorios" ? "principal-item current" : "principal-item"}>
+              <div className="group">
+                <BiDetail className="icon" size={20} />
+                <span>Relatórios</span>
+              </div>
+            </div>
+          </div>
+        </Link>
+
+
+
+        {/* <Link to="/dashboard/relatorios" className={ location === "/dashboard/relatorios" ? "item current": "item" }>
+          <div className="principal">
+            <BiDetail className="icon" size={20} />
+            <span>Relatórios</span>
+          </div>
+        </Link>
+ */}
+
+
+        {/* <Link to="/dashboard/relatorios" className={
           location === "/dashboard/relatorios" ? "item current": "item" 
         }>
           <BiDetail className="icon" size={20} />
           <span>Relatórios</span>
-        </Link>
+        </Link> */}
 
         {/* <Link to="/dashboard/criar-eventos" className={
           location === "/dashboard/criar-eventos" ? "item current": "item" 
