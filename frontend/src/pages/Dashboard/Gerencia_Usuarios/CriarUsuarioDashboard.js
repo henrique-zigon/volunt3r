e038a37f12470515942278244c8401401648cb1c
gabrielronny <gabrielronny123@hotmail.com>
2021-11-15 14:23:08 -0300

    adicionando criação do usuário via dashboard

--- frontend/src/pages/Dashboard/Gerencia_Usuarios/CriarUsuarioDashboard.js
+++ frontend/src/pages/Dashboard/Gerencia_Usuarios/CriarUsuarioDashboard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import api from '../../../api';
 import { useCookies } from 'react-cookie';
 import {
@@ -12,53 +12,115 @@ import HeaderWelcomePageDashboard from '../../../components/HeaderWelcomePageDas
 import InputForm from '../../../components/InputForm/InputForm';
 import NavBarDashboard from '../../../components/NavBarDashboard/NavBarDashboard';
 import { getURLApi } from '../../../configs/getUrlApi';
+import { useToasts } from 'react-toast-notifications';
+import { useHistory } from 'react-router';
 
 const CriarUsuarioDashboard = () => {
-	const [cookies] = useCookies(['volunt3r']);
+	const [cookies] = useCookies(['volunt3r_user']);
 	const [statePreview, setStatePreview] = useState();
 
+	const history = useHistory();
+	const { addToast } = useToasts();
+
+	const [userData, setUserData] = useState({
+		imagemPerfil: "",
+		imagemCapa: "",
+		nomeUsuario: "",
+		genero: "",
+		cargo: "",
+		area: "",
+		tipoUsuario: "",
+		email: "",
+		senha: "123"
+	});
+
 	const documentHandler = (e) => {
-        const reader = new FileReader();
-        reader.onload = () => {
-            if(reader.readyState === 2) {
-                setStatePreview(reader.result)
-            }
-        }
-        reader.readAsDataURL(e.target.files[0]);
-    }
+		const reader = new FileReader();
+		reader.onload = () => {
+			if (reader.readyState === 2) {
+				setStatePreview(reader.result)
+			}
+		}
+		reader.readAsDataURL(e.target.files[0]);
+	}
 
-    async function createNewUser(e) {
-        // PARA ENVIAR POST
-        e.preventDefault();
+	function handle(e) {
+		const newUserData = { ...userData }
+		newUserData[e.target.id] = e.target.value;
+		setUserData(newUserData);
+	}
 
-		if(statePreview == null){
-		// Usar formulário
+	async function submitForm(e) {
+		e.preventDefault();
+		const newUserData = { ...userData }
+
+		//Validação dos campos
+		var ValidaEmail = userData.email;
+		var ValidaSenha = userData.senha;
+		var RegexEmail = /@\w+.com/;
+		var TesteRegexEmail = RegexEmail.test(ValidaEmail);
+
+		if (
+			ValidaEmail === "" ||
+			ValidaSenha === "" ||
+			TesteRegexEmail === false
+		) {
+			addToast('Por favor, preencha todos os campos corretamente. O email deve conter @ e .com', { appearance: 'warning', autoDismiss: true });
+		}
+		else {
+			//Parte para enviar para a API
+			await api.post("/usuarios/novo", {
+				headers: {
+					'Authorization': cookies.volunt3r
+				},
+				data: newUserData
+			})
+				.then(resposta => {
+					if (resposta.status === 201) {
+						addToast('Usuário cadastrado com sucesso!', { appearance: 'success', autoDismiss: true })
+						history.push("/login")
+					}
+				}).catch((e) => {
+					if (e.response.status === 406) {
+						addToast('Ops... Seu usuário já existe', { appearance: 'error', autoDismiss: true })
+					}
+				});
+		}
+	}
+
+
+	async function createNewUser(e) {
+		// PARA ENVIAR POST
+		e.preventDefault();
+
+		if (statePreview == null) {
+			// Usar formulário
 		}
 
-		else{
-        
-        let formData = new FormData();
+		else {
 
-		console.log(e.target);
+			let formData = new FormData();
 
-        formData.append('arquivo', e.target.file_new_users.files[0]);
+			console.log(e.target);
 
-        let config = {
-            url: "/arquivos/upload",
-            method: "POST",
-            headers: {
-                'Authorization': cookies.volunt3r,
-                'Content-Type': 'multipart/form-data'
-            },
-            data: formData
-        }
+			formData.append('arquivo', e.target.file_new_users.files[0]);
 
-        await api(config).then((resposta) => {
-            console.log(resposta)
-        }).catch((e) => {
-            console.error(e)
-        });
-    }
+			let config = {
+				url: "/arquivos/upload",
+				method: "POST",
+				headers: {
+					'Authorization': cookies.volunt3r,
+					'Content-Type': 'multipart/form-data'
+				},
+				data: formData
+			}
+
+			await api(config).then((resposta) => {
+				console.log(resposta)
+			}).catch((e) => {
+				console.error(e)
+			});
+		}
 	}
 
 
@@ -77,7 +139,7 @@ const CriarUsuarioDashboard = () => {
 						subtitle="Que tal criar um usuário?"
 					/>
 
-					<form className="form-create" action="" onSubmit={createNewUser}>
+					<form className="form-create" onSubmit={(e) => submitForm(e)}>
 
 						<div className="group-form">
 							<InputForm
@@ -86,7 +148,7 @@ const CriarUsuarioDashboard = () => {
 								name="name"
 								label="Nome do usuário"
 								icon={<BiUser className="icon-input-group" />}
-							// function={(e) => handle(e)}
+								function={(e) => handle(e)}
 							/>
 
 
@@ -94,7 +156,7 @@ const CriarUsuarioDashboard = () => {
 								<label htmlFor="senha">
 									<span>Qual o gênero?</span>
 
-									<select className="input-field select" name="genero" id="genero">
+									<select className="input-field select" name="genero" id="genero" onChange={(e) => handle(e)}>
 										<option disabled selected>Selecione seu gênero</option>
 										<option value="F">Feminino</option>
 										<option value="M">Masculino</option>
@@ -112,12 +174,13 @@ const CriarUsuarioDashboard = () => {
 								name="cargo"
 								label="Cargo do usuário"
 								icon={<BiBuilding className="icon-input-group" />}
-							// function={(e) => handle(e)}
+								function={(e) => handle(e)}
 							/>
+
 							<div className="input-group">
 								<label htmlFor="area">
 									<span>Qual a área?</span>
-									<select className="input-field select" name="area" id="area">
+									<select className="input-field select" name="area" id="area" onChange={(e) => handle(e)}>
 										<option disabled selected>Selecione sua área</option>
 										<option value="B3 Social">B3 Social</option>
 										<option value="Produtos Analytics">Produtos Analytics</option>
@@ -136,13 +199,13 @@ const CriarUsuarioDashboard = () => {
 								name="email"
 								label="Qual o email do usuário?"
 								icon={<BiEnvelope className="icon-input-group" />}
-							// function={(e) => handle(e)}
+								function={(e) => handle(e)}
 							/>
 							<div className="input-group">
 								<label htmlFor="tipoUsuario">
 									<span>O usuário é B3 Social?</span>
 
-									<select className="input-field select" name="tipoUsuario" id="tipoUsuario">
+									<select className="input-field select" name="tipoUsuario" id="tipoUsuario" onChange={(e) => handle(e)}>
 										<option disabled selected >Selecione</option>
 										<option value="b3_social">Sim</option>
 										<option value="comum">Não</option>
@@ -172,36 +235,11 @@ const CriarUsuarioDashboard = () => {
 
 						<button type="submit" className="btn-new-submit">Cadastrar Novo Usuário</button>
 					</form>
-
-
-					
-
 				</div>
-
-
 			</main>
-
-
 		</div>
-		// <main className="container">
-		// 	<SideBar
-		// 		userpic={getURLApi() + "/arquivos/imagem/" + cookies.volunt3r_user.imagemPerfil}
-		// 		username={cookies.volunt3r_user.nomeUsuario}
-		// 		useremail={cookies.volunt3r_user.email}
-		// 	/>
-
-		// 	<div className="content">
-		// 		<HeaderWelcomePageDashboard
-		// 			username={cookies.volunt3r_user.nomeUsuario}
-		// 			subtitle="Que tal criar um usuário?"
-		// 		/>
-
-
-		// 	</div>
-		// </main>
 	);
 }
-
 
 export default CriarUsuarioDashboard;
 
